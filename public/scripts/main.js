c7ff64eeb97718015d9372ae50e0041a4d6179f2
Alice Xavier dos Santos <alicexaviers21@outlook.com>
2021-07-01 17:14:44 -0300

    adding modal open and close functionality

---
+++ public/scripts/main.js
@@ -0,0 +1,34 @@
+import Modal from './modal.js';
+
+const modal = Modal();
+
+const modalTitle = document.querySelector('.modal h2');
+const modalDescription = document.querySelector('.modal p');
+const modalButton = document.querySelector('.modal button');
+
+//pegar todos os botões que existe com a classe check
+const checkButtons = document.querySelectorAll('.actions a.check');
+
+checkButtons.forEach((button) => {
+  // adicionar a excuta
+  button.addEventListener('click', handleClick);
+});
+
+const deleteButton = document.querySelectorAll('.actions a.delete');
+
+deleteButton.forEach((button) => {
+  button.addEventListener('click', (event) => handleClick(event, false));
+});
+
+function handleClick(event, check = true) {
+  const text = check ? 'Marcar como lida' : 'Excluir';
+
+  modalTitle.innerHTML = `${text} esta pergunta`;
+  modalDescription.innerHTML = `Tem certeza que deseja ${text.toLocaleLowerCase()} esta pergunta?`;
+  modalButton.innerHTML = `Sim, ${text.toLocaleLowerCase()} `;
+  check
+    ? modalButton.classList.remove('red')
+    : modalButton.classList.add('red');
+
+  modal.open();
+}
