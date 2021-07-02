c29b673bc78775f74fd758c4d94000fe346bc26f
Alice Xavier dos Santos <alicexaviers21@outlook.com>
2021-07-02 15:44:07 -0300

    end of class 5

--- src/db/init.js
+++ src/db/init.js
@@ -1,10 +1,22 @@
-const database = require('./config');
+const Database = require('./config');
 
 const initDb = {
   async init() {
     const db = await Database();
 
-    await db.exec();
+    await db.exec(`CREATE TABLE rooms (
+            id INTEGER PRIMARY KEY,
+            pass TEXT
+        )`);
+
+    await db.exec(`CREATE TABLE questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            read INT,
+            room INT
+        )`);
+
+    await db.close();
   },
 };
 
